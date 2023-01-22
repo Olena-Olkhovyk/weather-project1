5f623b070ddd98dd63b54260eee838931d4d523d
AHBRTY56 <116176575+AHBRTY56@users.noreply.github.com>
2023-01-22 18:57:04 +0100

    Add temperature,humidity,wind

--- src/tech.js
+++ src/tech.js
@@ -20,3 +20,41 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let wetElement = document.querySelector("#wet");
+  wetElement.innerHTML = response.data.temperature.humidity;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let picElement = document.querySelector("#pic");
+  picElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  getForecast(response.data.coordinates);
+}
+
+function search(city) {
+  let apiKey = "bc7dota507232177ccef048eb1a1ae2a";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = cityInputElement.value;
+  search(cityInputElement.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
